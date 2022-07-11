f12baf1c4711619db47f1a3ce9dece9e9f60e59f
DanielVandH <danj.vandenheuvel@gmail.com>
2022-07-12 01:26:49 +1000

    move smoe stuff and add data field

--- src/problems.jl
+++ src/problems.jl
@@ -19,7 +19,7 @@ Returns the number of parameters being estimated for the likelihood/optimisation
 
 Returns the known parameters used for the likelihood problem `prob`.
 """
-@inline data(prob::AbstractLikelihoodProblem) = prob.prob.p
+@inline data(prob::AbstractLikelihoodProblem) = prob.data
 
 """
     names(prob::AbstractLikelihoodProblem)
@@ -58,149 +58,25 @@ end
 @doc (@doc bounds) @inline bounds(prob::AbstractLikelihoodProblem) = bounds(prob.prob)
 
 """
-    sym_names(prob::AbstractLikelihoodProblem)
+    finite_bounds(prob::OptimizationProblem)
+    finite_bounds(prob::AbstractLikelihoodProblem)
 
-Return the symbolic names used for the likelihood problem `prob`, or creates them 
-if none were provided in the construction of `prob`.
+Returns `true` if the given `prob` has finite lower and upper bounds, and `false`
+otherwise. Note that `nothing` bounds are interpreted as unbounded.
 """
-@inline function sym_names(prob::AbstractLikelihoodProblem)
-    nm = prob.prob.f.syms
-    !isnothing(nm) && return nm
-    rt = Vector{String}(undef, num_params(prob))
-    for i in 1:num_params(prob)
-        rt[i] = "θ" * subscriptnumber(i)
-    end
-    return rt
+function finite_bounds end
+function finite_bounds(prob::OptimizationProblem{iip,FF,θType,P,B,LC,UC,Sns,K}) where {iip,AD,G,H,HV,C,CJ,CH,HP,CJP,CHP,S,HCV,CJCV,CHCV,EX,CEX,F,FF<:OptimizationFunction{iip,AD,F,G,H,HV,C,CJ,CH,HP,CJP,CHP,S,HCV,CJCV,CHCV,EX,CEX},θType,P,B,LC,UC,Sns,K}
+    all(isfinite, prob.lb) && all(isfinite, prob.ub)
+end
+function finite_bounds(prob::OptimizationProblem{iip,FF,θType,P,Nothing,LC,UC,Sns,K}) where {iip,AD,G,H,HV,C,CJ,CH,HP,CJP,CHP,S,HCV,CJCV,CHCV,EX,CEX,F,FF<:OptimizationFunction{iip,AD,F,G,H,HV,C,CJ,CH,HP,CJP,CHP,S,HCV,CJCV,CHCV,EX,CEX},θType,P,LC,UC,Sns,K}
+    false
+end
+function finite_bounds(prob::AbstractLikelihoodProblem)
+    finite_bounds(prob.prob)
 end
 
 """
-    choose_θ₀(::Nothing, lb, ub, num_params)
-    choose_θ₀(θ₀::T, args...) where {T<:AbstractVector}
-
-Chooses the initial guess `θ₀` based on the provided lower bounds `lb` and upper bounds 
-`ub`, with a number of parameters `num_params`, if no `θ₀` is provided. Otherwise, simply 
-returns `θ₀`.
-"""
-function choose_θ₀ end
-@inline function choose_θ₀(::Nothing, lb, ub, num_params)
-    if isnothing(lb) && isnothing(ub)
-        θ₀ = zeros(num_params)
-    elseif !isnothing(lb) && !isnothing(ub)
-        if all(isfinite, lb) && all(isfinite, ub)
-            θ₀ = similar(lb)
-            for i in eachindex(θ₀)
-                θ₀[i] = 0.5(lb[i] + ub[i])
-            end
-        elseif all(isfinite, lb) && !all(isfinite, ub)
-            θ₀ = copy(lb)
-        elseif !all(isfinite, lb) && all(isfinite, ub)
-            θ₀ = copy(ub)
-        else
-            θ₀ = zeros(num_params)
-        end
-    else
-        error("If any of `lb` or `ub` is provided, both must be provided.")
-    end
-    return θ₀
-end
-@inline choose_θ₀(θ₀::T, args...) where {T<:AbstractVector} = θ₀
-
-"""
-    negate_loglik(loglik)
-    negate_loglik(loglik, integrator)
-
-Returns `(θ, p) -> -loglik(θ, p)`, or `(θ, p) -> -loglik(θ, p, integrator)` if an 
-`integrator` is provided.
-"""
-function negate_loglik end
-@inline negate_loglik(loglik) = @inline (θ, p) -> -loglik(θ, p)
-@inline negate_loglik(loglik, integrator) = @inline (θ, p) -> -loglik(θ, p, integrator)
-
-"""
-    construct_optimisation_function(negloglik, adtype,
-        grad, hess, hv,
-        cons, cons_j, cons_h,
-        hess_prototype, cons_jac_prototype, cons_hess_prototype,
-        syms,
-        hess_colorvec, cons_jac_colorvec, cons_hess_colorvec,
-        expr, cons_expr, f_kwargs)
-
-Constructs an optimisation function with objective `negloglik`. 
-See [`OptimizationFunction`](@ref).
-"""
-@inline function construct_optimisation_function(negloglik, adtype,
-    grad, hess, hv,
-    cons, cons_j, cons_h,
-    hess_prototype, cons_jac_prototype, cons_hess_prototype,
-    syms,
-    hess_colorvec, cons_jac_colorvec, cons_hess_colorvec,
-    expr, cons_expr, f_kwargs)
-    optfcall = (
-        grad=grad, hess=hess, hv=hv,
-        cons=cons, cons_j=cons_j, cons_h=cons_h,
-        hess_prototype=hess_prototype, cons_jac_prototype=cons_jac_prototype, cons_hess_prototype=cons_hess_prototype,
-        syms=syms,
-        hess_colorvec=hess_colorvec, cons_jac_colorvec=cons_jac_colorvec, cons_hess_colorvec=cons_hess_colorvec,
-        expr=expr, cons_expr=cons_expr
-    )
-    optfcall_cleaned = clean_named_tuple(optfcall)
-    if !isnothing(optfcall_cleaned)
-        if isnothing(f_kwargs)
-            f = OptimizationFunction(negloglik, adtype; optfcall_cleaned...)
-        else
-            f = OptimizationFunction(negloglik, adtype; optfcall_cleaned..., f_kwargs...)
-        end
-    else
-        if isnothing(f_kwargs)
-            f = OptimizationFunction(negloglik, adtype)
-        else
-            f = OptimizationFunction(negloglik, adtype; f_kwargs...)
-        end
-    end
-    return f
-end
-
-"""
-    construct_optimisation_problem(f, θ₀, data,
-        lb, ub, 
-        lcons, ucons, 
-        sense, 
-        prob_kwargs)
-
-Constructs an optimisation problem. See [`OptimizationProblem`](@ref).
-"""
-@inline function construct_optimisation_problem(f, θ₀, data,
-    lb, ub,
-    lcons, ucons,
-    sense,
-    prob_kwargs)
-    optprobcall = (
-        lb=lb, ub=ub,
-        lcons=lcons, ucons=ucons,
-        sense=sense
-    )
-    optprobcall_cleaned = clean_named_tuple(optprobcall)
-    if !isnothing(optprobcall_cleaned)
-        if isnothing(prob_kwargs)
-            prob = OptimizationProblem(f, θ₀, data; optprobcall_cleaned...)
-        else
-            prob = OptimizationProblem(f, θ₀, data; optprobcall_cleaned..., prob_kwargs...)
-        end
-    else
-        if isnothing(prob_kwargs)
-            prob = OptimizationProblem(f, θ₀, data)
-        else
-            prob = OptimizationProblem(f, θ₀, data; prob_kwargs...)
-        end
-    end
-    return prob
-end
-
-################################################################################
-## Likelihood problem 
-################################################################################
-"""
-    LikelihoodProblem{ST,iip,F,θType,P,B,LC,UC,S,K,θ₀Type,ℓ<:Function} <: AbstractLikelihoodProblem
+    LikelihoodProblem{ST,iip,F,θType,P,B,LC,UC,S,K,D,θ₀Type,ℓ<:Function} <: AbstractLikelihoodProblem
 
 A struct that is used to define a likelihood problem. 
 
@@ -216,6 +92,9 @@ The optimisation problem, with `OptimizationProblem` coming from `Optimization.j
     (log-)likelihood function in the objective function, but keep `loglik` as documented below.
     See also [`negate_loglik`](@ref).
 
+- `data::D`
+
+The data provided for the optimization problem, i.e. the argument `p` in `loglik(θ, p)` (see below).
 - `loglik::F`
 
 This is the log-likelihood function. Do not apply a negative to this function, it is done by the 
@@ -300,8 +179,9 @@ The third constructor also allows for the following two keyword arguments.
 - `ode_alg=nothing`: Algorithm to use for solving the ODEs. If `nothing`, one is chosen automatically.
 - `ode_kwargs=nothing`: Additional keyword arguments for the integrator interface.
 """
-Base.@kwdef struct LikelihoodProblem{ST,iip,F,θType,P,B,LC,UC,S,K,θ₀Type,ℓ<:Function} <: AbstractLikelihoodProblem
+Base.@kwdef struct LikelihoodProblem{ST,iip,F,θType,P,B,LC,UC,S,K,D,θ₀Type,ℓ<:Function} <: AbstractLikelihoodProblem
     prob::OptimizationProblem{iip,F,θType,P,B,LC,UC,S,K}
+    data::D
     loglik::ℓ
     θ₀::θ₀Type
     names::ST
@@ -326,10 +206,10 @@ function LikelihoodProblem(
         error("The length of `lb` must equal `num_params = $num_params`.")
     end
     if !isnothing(ub) && num_params ≠ length(ub)
-        error("The length of `lb` must equal `num_params = $num_params`.")
+        error("The length of `ub` must equal `num_params = $num_params`.")
     end
     if !isnothing(θ₀) && length(θ₀) ≠ num_params
-        error("The length of `lb` must equal `num_params = $num_params`.")
+        error("The length of `θ₀` must equal `num_params = $num_params`.")
     end
     θ = choose_θ₀(θ₀, lb, ub, num_params)
     negloglik = negate_loglik(loglik)
