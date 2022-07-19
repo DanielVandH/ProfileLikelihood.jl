873d5e0c3815619b00a3aa0aa6fc906153f81a55
DanielVandH <danj.vandenheuvel@gmail.com>
2022-07-20 00:58:48 +1000

    prep for auto

--- test/template_functions.jl
+++ test/template_functions.jl
@@ -179,3 +179,45 @@ function LogisticODE()
     @inferred prob.prob.f(prob.θ₀, ProfileLikelihood.data(prob))
     return prob, loglik, (λ, K, σ, u₀), uᵒ, n
 end
+
+
+function LinearExponentialODEAutoDiff()
+    Random.seed!(2992999)
+    λ = -0.5
+    y₀ = 15.0
+    σ = 0.1
+    T = 5.0
+    n = 200
+    Δt = T / n
+    t = [j * Δt for j in 0:n]
+    y = y₀ * exp.(λ * t)
+    yᵒ = y .+ [0.0, rand(Normal(0, σ), n)...]
+    function ode_fnc(u, p, t)
+        local λ
+        λ = p
+        du = λ * u
+        return du
+    end
+    function loglik(θ, data, integrator)
+        local yᵒ, n
+        ## Extract the parameters
+        yᵒ, n = data
+        #λ, σ, u0 = θ
+        ## What do you want to do with the integrator?
+        integrator.p = θ[1]
+        ## Now solve the problem 
+        reinit!(integrator, θ[3])
+        solve!(integrator) # DON'T DO SOL = ... IT CAN CAUSE MEMORY ISSUES OR CHANGE THE REF FOR FUTURE REFERENCES TO VARIABLES CALLED "sol"
+        ## Now what do you want to do with sol?
+        return gaussian_loglikelihood(yᵒ, integrator.sol.u, θ[2], n)
+    end
+    θ₀ = [-1.0, 0.5, 19.73]
+    prob = LikelihoodProblem(loglik, 3, ode_fnc, y₀, (0.0, T), 1.0, t;
+        data=(yᵒ, n), θ₀, lb=[-10.0, 1e-6, 0.5], ub=[10.0, 10.0, 25.0], ode_kwargs=(verbose=false,),
+        names=[L"\lambda", L"\sigma", L"y_0"],
+        syms=[:λ, :σ, :y₀])
+    integrator = setup_integrator(ode_fnc, y₀, (0.0, T), 1.0, t)
+    @inferred loglik(prob.θ₀, ProfileLikelihood.data(prob), integrator)
+    @inferred prob.prob.f(prob.θ₀, ProfileLikelihood.data(prob))
+    return prob, loglik, (λ, σ, y₀), yᵒ, n
+end
